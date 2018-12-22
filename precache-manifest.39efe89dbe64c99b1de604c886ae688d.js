5e7fe515358d889dbcf3c7988f77dbb012b67b99
Bruce Harms <bruceh_88@comcast.net>
2018-12-22 11:25:16 -0500

    Updates

---
+++ precache-manifest.39efe89dbe64c99b1de604c886ae688d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a70c675f9a23daa38fda",
+    "url": "/clickygame/static/js/runtime~main.a70c675f.js"
+  },
+  {
+    "revision": "2ba9252e8ae95cb6d1ab",
+    "url": "/clickygame/static/js/main.2ba9252e.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/clickygame/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "2ba9252e8ae95cb6d1ab",
+    "url": "/clickygame/static/css/main.ee5cc910.chunk.css"
+  },
+  {
+    "revision": "0e9309b978cc14ec95cc7c231383719e",
+    "url": "/clickygame/index.html"
+  }
+];
